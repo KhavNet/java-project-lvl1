4c1466ee696f427f5e89f9f28530b69a348810ee
khav <dio-st@mail.ru>
2022-04-26 02:39:16 +0300

    add workflow readme

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -1,2 +1,13 @@
-package hexlet.code;public class Cli {
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void getName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+        sc.close();
+    }
 }
